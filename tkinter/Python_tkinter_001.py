cc08880017e51b8fa224e612b957586539d6817c
Youngjae <dudwo56@gmail.com>
2017-01-20 13:56:43 +0900

    tkinter tk

---
+++ tkinter/Python_tkinter_001.py
@@ -0,0 +1,8 @@
+# -*- encoding:utf8 -*-
+# Python version in the development environment 2.7.11
+import os
+os.chdir(os.path.dirname(__file__))
+from Tkinter import *
+root = Tk()
+root.title("simple GUI")
+root.mainloop()

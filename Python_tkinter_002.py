35151f1fb7bff0a0e9defc41758df6421ed71267
Youngjae <dudwo56@gmail.com>
2017-01-20 14:46:48 +0900

    -

---
+++ Python_tkinter_002.py
@@ -0,0 +1,10 @@
+# -*- encoding:utf8 -*-
+# Python version in the development environment 2.7.11
+import os
+os.chdir(os.path.dirname(__file__))
+from Tkinter import *
+root = Tk()
+def closecallback():
+    root.destroy()
+Button(root,text="Close",command=closecallback).pack()
+root.mainloop()
